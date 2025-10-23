0e475e387f9a3211205947f4a6f57561e05a42e9
unknown <dayren86@gmial.com>
2025-10-23 13:42:09 +0300

    read file hw v1

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWork {
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> strings;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            strings = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String string : strings) {
+            String[] split = string.split("\\W+");
+            for (String s : split) {
+                String lowerCase = s.toLowerCase();
+                if (lowerCase.charAt(0) == 119) {
+                    stringBuilder.append(lowerCase).append(" ");
+                }
+            }
+        }
+
+        String[] split = {};
+        if (!stringBuilder.isEmpty()) {
+            split = stringBuilder.toString().split(" ");
+            Arrays.sort(split);
+        }
+
+        return split;
     }
 }
